9b58376f6359a28380f9fe7d5ec46ebdb2660e07
yuu-th <88813495+yuu-th@users.noreply.github.com>
2026-03-26 17:52:14 +0900

    npm install -g を有効化するための設定を適用

--- modules/dev/home.nix
+++ modules/dev/home.nix
@@ -8,4 +8,14 @@
     python3
     nodejs_22
   ];
+  
+  # npm prefix を Nix store 外に向ける
+  home.file.".npmrc".text = ''
+    prefix=''${HOME}/.npm-global
+  '';
+
+  # ~/.npm-global/bin を PATH に追加
+  home.sessionPath = [
+    "$HOME/.npm-global/bin"
+  ];
 }
